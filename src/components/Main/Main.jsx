1f3e4a2d89a925b7559ed2659634fc1456f30571
Vladimir Blinov <zemetrans21@yandex.ru>
2021-10-06 17:00:44 +0300

    feat: разместить элементы компонента AboutProject

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './Main.css'
 import Header from '../Header/Header'
 import Promo from '../Promo/Promo'
+import AboutProject from '../AboutProject/AboutProject'
 
 export default function Main() {
   return (
     <>
       <Header isLogin={false} />
       <Promo />
+      <AboutProject />
     </>
   )
 }
