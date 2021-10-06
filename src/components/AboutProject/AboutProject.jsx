422f0e01d59faeb3f0e45d0a3d65729c021f3564
Vladimir Blinov <zemetrans21@yandex.ru>
2021-10-06 17:40:44 +0300

    feat: стилизовать часть AboutProject

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -8,25 +8,25 @@ export default function AboutProject() {
       <div className="about-project__stage-container">
         <div className="about-project__stage">
           <h3 className="about-project__stage-title">Дипломный проект включал 5 этапов</h3>
-          <p clasName="about-project__stage-subtitle">
+          <p className="about-project__stage-subtitle">
             Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
           </p>
         </div>
         <div className="about-project__stage">
           <h3 className="about-project__stage-title">На выполнение диплома ушло 5 недель</h3>
-          <p clasName="about-project__stage-subtitle">
+          <p className="about-project__stage-subtitle">
             У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
           </p>
         </div>
-        <div className="about-project__timeline-container">
-          <div className="about-project__timeline-part">
-            <p className="about-project__timeline-time">1 неделя</p>
-            <p className="about-project__timeline-description">Back-end</p>
-          </div>
-          <div className="about-project__timeline-part">
-            <p className="about-project__timeline-time">4 неделя</p>
-            <p className="about-project__timeline-description">Front-end</p>
-          </div>
+      </div>
+      <div className="about-project__timeline-container">
+        <div className="about-project__timeline-part">
+          <p className="about-project__timeline-time">1 неделя</p>
+          <p className="about-project__timeline-description">Back-end</p>
+        </div>
+        <div className="about-project__timeline-part">
+          <p className="about-project__timeline-time">4 неделя</p>
+          <p className="about-project__timeline-description">Front-end</p>
         </div>
       </div>
     </section>
