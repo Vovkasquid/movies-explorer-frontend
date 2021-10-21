8c3d49513f475410d20a3aaed03d8bb5940cb618
Vladimir Blinov <zemetrans21@yandex.ru>
2021-10-21 17:15:01 +0300

    feat: сделать десктоп версию Movies

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import './MoviesCard.css'
-import filmPicture from '../../images/film-example.png'
 
-export default function MoviesCard() {
+export default function MoviesCard({ filmName, filmDuration, filmPicture, isLiked }) {
   return (
     <div className="movies-card">
       <div className="movies-card__info-container">
-        <h3 className="movies-card__title">33 слова о дизайне</h3>
-        <p className="movies-card__duration">1ч 42м</p>
+        <h3 className="movies-card__title">{filmName}</h3>
+        <p className="movies-card__duration">{filmDuration}</p>
         <button
           aria-label="like"
           type="button"
-          className="movies-card__like-button movies-card__like-button_status_active"
+          className={
+            isLiked ? `movies-card__like-button movies-card__like-button_status_active` : `movies-card__like-button`
+          }
         />
       </div>
       <img className="movies-card__film-picture" alt="картинка фильма" src={filmPicture} />
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,13 +1,47 @@
 import React from 'react'
 import './MoviesCardList.css'
 import MovieCard from '../MoviesCard/MoviesCard'
+import filmPicture from '../../images/film-example.png'
 
 export default function MoviesCardList() {
   return (
     <section className="movies-card-list">
       <ul className="movies-card-list__list">
         <li>
-          <MovieCard />
+          <MovieCard filmName="33 слова о дизайне" filmDuration="1ч 42м" filmPicture={filmPicture} isLiked />
+        </li>
+        <li>
+          <MovieCard
+            filmName="Киноальманах «100 лет дизайна»"
+            filmDuration="1ч 42м"
+            filmPicture={filmPicture}
+            isLiked
+          />
+        </li>
+        <li>
+          <MovieCard filmName="В погоне за Бенкси" filmDuration="1ч 42м" filmPicture={filmPicture} isLiked={false} />
+        </li>
+        <li>
+          <MovieCard
+            filmName="Баския: Взрыв реальности"
+            filmDuration="1ч 42м"
+            filmPicture={filmPicture}
+            isLiked={false}
+          />
+        </li>
+        <li>
+          <MovieCard filmName="Бег это свобода" filmDuration="1ч 42м" filmPicture={filmPicture} isLiked />
+        </li>
+        <li>
+          <MovieCard filmName="Книготорговцы" filmDuration="1ч 42м" filmPicture={filmPicture} isLiked />
+        </li>
+        <li>
+          <MovieCard
+            filmName="Когда я думаю о Германии ночью"
+            filmDuration="1ч 42м"
+            filmPicture={filmPicture}
+            isLiked={false}
+          />
         </li>
       </ul>
       <button type="button" className="movies-card-list__next-films-button">
