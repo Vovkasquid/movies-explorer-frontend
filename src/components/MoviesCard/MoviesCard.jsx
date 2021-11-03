e5efd9773c8f108dbde1e1b003be57baef3e9ef3
Vladimir Blinov <zemetrans21@yandex.ru>
2021-11-03 12:58:15 +0300

    feat: сделать заготовку под кнопку удаления карточки с фильмом

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -7,13 +7,24 @@ export default function MoviesCard({ filmName, filmDuration, filmPicture, isLike
       <div className="movies-card__info-container">
         <h3 className="movies-card__title">{filmName}</h3>
         <p className="movies-card__duration">{filmDuration}</p>
-        <button
-          aria-label="like"
-          type="button"
-          className={
-            isLiked ? `movies-card__like-button movies-card__like-button_status_active` : `movies-card__like-button`
-          }
-        />
+        {1 && (
+          <button
+            aria-label="delete"
+            type="button"
+            className="movies-card__button movies-card__button_type_close-btn"
+          />
+        )}
+        {null && (
+          <button
+            aria-label="like"
+            type="button"
+            className={
+              isLiked
+                ? `movies-card__button movies-button_type_active-like-btn`
+                : `movies-card__button movies-button_type_disabled-like-btn`
+            }
+          />
+        )}
       </div>
       <img className="movies-card__film-picture" alt="картинка фильма" src={filmPicture} />
     </div>
