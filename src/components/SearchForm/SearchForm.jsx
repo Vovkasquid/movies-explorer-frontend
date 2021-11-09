2417eaa2fa0573addbaaca129fa53439d7475b9e
Vladimir Blinov <zemetrans21@yandex.ru>
2021-11-09 13:17:21 +0300

    feat: добавить первую версию валидации. Вероятно придётся изменить её логику в searform

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
+import formValidationHook from '../../hooks/formValidationHook'
 import './SearchForm.css'
 
 export default function SearchForm() {
+  const { values, isValid, handleChange } = formValidationHook({
+    search: '',
+  })
+
   return (
     <section className="search-form">
-      <form className="search-form__form" name="search">
+      <form className="search-form__form" name="search" noValidate>
         <div className="search-form__input-fields">
-          <input placeholder="Фильм" type="search" required className="search-form__input-field" />
+          <input
+            placeholder="Фильм"
+            type="search"
+            required
+            className="search-form__input-field"
+            onChange={handleChange}
+            value={values.search}
+          />
           <button aria-label="найти фильмы" type="submit" className="search-form__form-submit" />
         </div>
+        <span
+          className={
+            isValid
+              ? 'search-form__input-error-text'
+              : 'search-form__input-error-text search-form__input-error-text_active'
+          }
+        >
+          Нужно ввести ключевое слово
+        </span>
         <label htmlFor="short-films" className="search-form__checkbox-button-label">
           <input
             id="short-films"
