85933c116256c04b4d5593275073a96eebbabf47
Vladimir Blinov <zemetrans21@yandex.ru>
2021-11-10 17:53:11 +0300

    fix: кнопка ещё не появляется когда все карточки уже показаны

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -5,18 +5,16 @@ import MovieCard from '../MoviesCard/MoviesCard'
 export default function MoviesCardList({ isSaved, movies, dataLength, renderCounter, setRenderCounter }) {
   const filmDuration = (movie) => `${Math.floor(movie.duration / 60)}ч ${movie.duration % 60}м`
   const renderArray = movies.slice(0, renderCounter)
-  const [isBtnVisible, setIsBtnVisible] = React.useState(!(dataLength - renderCounter < 7))
+  const [isBtnVisible, setIsBtnVisible] = React.useState(!(dataLength - renderCounter <= 7))
   const handleAddingBtn = () => {
     // проверяем может ли мы ещё добавить полное количество карточек
     if (dataLength - renderCounter < 7) {
       setRenderCounter(renderCounter + (dataLength - renderCounter))
+      setIsBtnVisible(false)
       console.log(renderCounter)
     } else {
       setRenderCounter(renderCounter + 7)
       console.log(renderCounter)
-    }
-    if (renderCounter >= dataLength) {
-      setIsBtnVisible(false)
     }
   }
   return (
